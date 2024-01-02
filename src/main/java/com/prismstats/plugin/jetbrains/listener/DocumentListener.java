9880548436629269dd18b270cad2a23d75f83715
slxca <straeter.luca@gmail.com>
2024-01-02 20:20:41 +0100

    Refactor document change event handling logic
    Simplified the logic inside the documentChangedNonBulk method in DocumentListener for clarity, efficiency, and readability. Instead of storing old values and calculating changes, the updated code makes decisions based on event differences, eliminating unnecessary operations. This also improves thread-safety by reducing shared state manipulation.

--- src/main/java/com/prismstats/plugin/jetbrains/listener/DocumentListener.java
+++ src/main/java/com/prismstats/plugin/jetbrains/listener/DocumentListener.java
@@ -1,5 +1,5 @@
 package com.prismstats.plugin.jetbrains.listener;
-import com.intellij.openapi.editor.Document;
+
 import com.intellij.openapi.editor.event.BulkAwareDocumentListener;
 import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.project.Project;
@@ -9,48 +9,37 @@ import com.prismstats.plugin.jetbrains.collectors.FileCollector;
 import com.prismstats.plugin.jetbrains.collectors.ProjectCollector;
 import org.jetbrains.annotations.NotNull;
 public class DocumentListener implements BulkAwareDocumentListener.Simple {
-    public static int oldLineCount = 0;
-    public static int oldCharCount = 0;
+
     @Override
     public void documentChangedNonBulk(@NotNull DocumentEvent documentEvent) {
+        documentEvent.getOldFragment().length();
+
         try {
             Project project = PrismStats.getProject(documentEvent.getDocument());
             if(project == null) return;
 
-            Document document = documentEvent.getDocument();
-
-            String newText = document.getText();
-            int newLineCount = document.getLineCount();
-            int newCharCount = Math.toIntExact(newText.chars().count());
-            collectData(newLineCount, newCharCount, oldLineCount, oldCharCount);
-            oldLineCount = newLineCount;
-            oldCharCount = newCharCount;
             FileCollector.addFile(documentEvent);
             ProjectCollector.addProject(project);
             ProjectCollector.addProjectFile(project, documentEvent);
+
+            if(documentEvent.getOldFragment().toString().isEmpty()) {
+                // CHAR ADDED
+                DataCollector.addChar(documentEvent.getNewFragment().toString().length());
+            } else if(documentEvent.getNewFragment().toString().isEmpty()) {
+                // CHAR REMOVED
+                DataCollector.removeChar(documentEvent.getOldFragment().toString().length());
+            }
+
+            if(documentEvent.getNewFragment().toString().equals("\n")) {
+                // LINE ADDED
+                DataCollector.addLine(1);
+            } else if(documentEvent.getOldFragment().toString().equals("\n")) {
+                // LINE REMOVED
+                DataCollector.removeLine(1);
+            }
+
         } catch (Exception e) {
             System.out.println("Error while collecting data: " + e.getMessage());
-        }
-    }
-
-    private void collectData(int newLineCount, int newCharCount, int oldLineCount, int oldCharCount) {
-        int addedLines = newLineCount - oldLineCount;
-        int removedLines = oldLineCount - newLineCount;
-        int addedChars = newCharCount - oldCharCount;
-        int removedChars = oldCharCount - newCharCount;
-        updateDataCollector(addedLines, removedLines, addedChars, removedChars);
-    }
-
-    private void updateDataCollector(int addedLines, int removedLines, int addedChars, int removedChars) {
-        if(addedLines > 0) {
-            DataCollector.addLine(addedLines);
-        } else if (removedLines > 0) {
-            DataCollector.removeLine(removedLines);
-        }
-        if(addedChars > 0) {
-            DataCollector.addChar(addedChars);
-        } else if (removedChars > 0) {
-            DataCollector.removeChar(removedChars);
         }
     }
 }
